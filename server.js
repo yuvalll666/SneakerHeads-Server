ecdded6866842e3d15c58d4995b4e074a8426be2
Yuval Azaria <yuval66550@gmail.com>
2020-12-31 13:13:34 +0200

    third commit

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3900;
+
+mongoose
+  .connect(
+    `mongodb+srv://yuval:315569533@onlineshop.abqhu.mongodb.net/online-shop?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => console.log("connected to mongoose successfuly!!"))
+  .catch(() => console.log("Failed to connect to mongoose"));
 
 app.use(require("morgan")("dev"));
 app.use(express.json());
@@ -15,6 +28,8 @@ const koko = {
   lastName: "azarya",
   email: "yuval@gmail.com",
 };
+
+console.log(koko);
 
 app.get("/koko", (req, res) => {
   if (koko) {
