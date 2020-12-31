83c10e3a7cb63e8bd4e786a9a5a4f45d25c269b1
Yuval Azaria <yuval66550@gmail.com>
2020-12-31 16:07:19 +0200

    all changes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const localUrl = "http://localhost:3000";
-const apiUrl = "http://localhost:3900";
+const apiUrl = "https://sneakerheadsfreshstart.herokuapp.com/";
 
 module.exports = {
   localUrl,
