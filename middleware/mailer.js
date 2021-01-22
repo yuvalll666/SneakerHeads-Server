9803e414f81380b3169ce2abf6bfec829edabaad
Yuval Azaria <yuval66550@gmail.com>
2021-01-22 19:00:19 +0200

    del line 2 in mailer

--- middleware/mailer.js
+++ middleware/mailer.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config();
+// require("dotenv").config();
 
 async function sendIt(user, url) {
   let transporter = nodemailer.createTransport({
