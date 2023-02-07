7e20ec181481be0a6c823730a480eee90a6f943f
Yuval Azaria <yuval66550@gmail.com>
2023-02-07 17:30:36 +0200

    fix

--- server.js
+++ server.js
@@ -10,21 +10,19 @@ const path = require("path");
 const PORT = process.env.PORT || 3900;
 const { Product } = require("./models/product");
 
-// "heroku-postbuild": "npm run client:install && npm run run client:build"
-
 // Connect to atlas mongoDB
 mongoose
-  .connect(
-    `mongodb+srv://yuval:${process.env.MONGO_PASSWORD}@onlineshop.abqhu.mongodb.net/online-shop?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  )
-  .then(() => console.log("connected to mongoose successfuly!!"))
-  .catch(() => console.log(err, "Failed to connect to mongoose", err));
+    .connect(
+        `mongodb+srv://yuval:${process.env.MONGO_PASSWORD}@onlineshop.abqhu.mongodb.net/online-shop?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        }
+    )
+    .then(() => console.log("connected to mongoose successfuly!!"))
+    .catch((err) => console.log(err, "Failed to connect to mongoose", err));
 
 // Allow access from diffrent ports
 app.use(cors());
@@ -38,45 +36,45 @@ app.use("/api/products", require("./routes/products"));
 
 // Updates user to confiremed user
 app.get("/confirmation/:token", async (req, res) => {
-  try {
-    // Verify EMAIL_SECRET exists in token
-    const data = jwt.verify(req.params.token, process.env.EMAIL_SECRET);
-    if (data) {
-      // Update user
-      await User.findOneAndUpdate({ _id: data._id }, { confirmed: true });
-      // Redirect to confirmation page
-      return res.redirect(`${localUrl}/confirmation`);
-    } else {
-      return res.send("Error");
+    try {
+        // Verify EMAIL_SECRET exists in token
+        const data = jwt.verify(req.params.token, process.env.EMAIL_SECRET);
+        if (data) {
+            // Update user
+            await User.findOneAndUpdate({ _id: data._id }, { confirmed: true });
+            // Redirect to confirmation page
+            return res.redirect(`${localUrl}/confirmation`);
+        } else {
+            return res.send("Error");
+        }
+    } catch (e) {
+        return res.send({ error: e });
     }
-  } catch (e) {
-    return res.send({ error: e });
-  }
 });
 
 // This middleware informs the express application to serve our compiled React files
 if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging"
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "staging"
 ) {
-  app.use(express.static("client/build"));
-  // app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static("client/build"));
+    // app.use(express.static(path.join(__dirname, "client/build")));
 
-  // app.get("*", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  // });
+    // app.get("*", function (req, res) {
+    //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    // });
 }
 
 // Catch any bad requests
 app.get("*", (req, res) => {
-  res.status(200).json({
-    msg: "Catch All",
-  });
+    res.status(200).json({
+        msg: "Catch All",
+    });
 });
 
 // Listen on port 3900
 app.listen(PORT, () =>
-  console.log(
-    `Listening on port: ${PORT}.. Click me -> http://localhost:${PORT}`
-  )
+    console.log(
+        `Listening on port: ${PORT}.. Click me -> http://localhost:${PORT}`
+    )
 );
